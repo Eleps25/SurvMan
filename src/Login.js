33a3682af702756c22fc372bdcb8225d219c72d8
Jakub Mezulanik <mezuja@gmail.com>
2023-06-04 13:10:27 +0200

    Login refactor

--- src/Login.js
+++ src/Login.js
@@ -1,78 +1,80 @@
-import React, { Component } from "react";
-import logo from "./sources/Logo.PNG"
+import React, { useState } from "react";
+import logo from "./sources/Logo.PNG";
 import { Navigate } from "react-router-dom";
 import "./css/Login.css";
 import "./css/NewColors.css";
 
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: "",
-            password: "",
-            isLoggedIn: false
-        };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-    }
+const RIGHT_PASSWORD = "admin123";
+const RIGHT_USERNAME = "admin";
 
-    handleLogin() {
-        this.setState(prevState => ({ isLoggedIn: !prevState.isLoggedIn }));
-    }
+const Login = (props) => {
+  const [userInputData, setUserInputData] = useState({
+    username: "",
+    password: "",
+  });
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    handleSubmit(evt) {
-        evt.preventDefault();
-        let rightPassword = "admin123";
-        let rightUsername = "admin";
-        if (this.state.username !== rightUsername || this.state.password !== rightPassword) {
-            alert("Zadány špatné přihlašovací údaje");
-        } else {
-            this.handleLogin();
-            console.log("Logged in");
-        }
-    }
+  const handleLogin = () => {
+    setIsLoggedIn((prevState) => !prevState);
+  };
 
-    handleChange(evt) {
-        this.setState({
-            [evt.target.name]: evt.target.value
-        });
-    };
-
-    render() {
-        return (
-            <div className="login-body">
-                <div className="login-main">
-                    <img src={logo} alt="Logo společnosti" style={{ width: "25vw", borderRadius: '0.3em' }} />
-                    <p className="fw-bold">Zadejte prosím vaše přihlašovací údaje</p>
-                    {this.state.isLoggedIn && <Navigate to="/dashboard" replace={true} />}
-                    <form onSubmit={this.handleSubmit} className="login-form">
-                        <div className="login-form-inputs">
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="Uživatelské jméno"
-                                required="required"
-                                value={this.state.username}
-                                onChange={this.handleChange}
-                                className="login-form-item"
-                            />
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Heslo"
-                                required="required"
-                                value={this.state.password}
-                                onChange={this.handleChange}
-                                className="login-form-item"
-                            />
-                        </div>
-                        <button className="btn bgcolor-primary-new login-form-item"><span className="text-light fw-bold">PŘIHLÁSIT SE</span></button>
-                    </form>
-                </div>
-            </div>
-        )
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (
+      userInputData.username !== RIGHT_USERNAME ||
+      userInputData.password !== RIGHT_PASSWORD
+    ) {
+      alert("Zadány špatné přihlašovací údaje");
+    } else {
+      handleLogin();
+      console.log("Logged in");
     }
-}
+  };
+
+  const handleChange = (evt) => {
+    setUserInputData((prevData) => {
+      return { ...prevData, [evt.target.name]: evt.target.value };
+    });
+  };
+
+  return (
+    <div className="login-body">
+      <div className="login-main">
+        <img
+          src={logo}
+          alt="Logo společnosti"
+          style={{ width: "25vw", borderRadius: "0.3em" }}
+        />
+        <p className="fw-bold">Zadejte prosím vaše přihlašovací údaje</p>
+        {isLoggedIn && <Navigate to="/dashboard" replace={true} />}
+        <form onSubmit={handleSubmit} className="login-form">
+          <div className="login-form-inputs">
+            <input
+              type="text"
+              name="username"
+              placeholder="Uživatelské jméno"
+              required="required"
+              value={userInputData.username}
+              onChange={handleChange}
+              className="login-form-item"
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Heslo"
+              required="required"
+              value={userInputData.password}
+              onChange={handleChange}
+              className="login-form-item"
+            />
+          </div>
+          <button className="btn bgcolor-primary-new login-form-item">
+            <span className="text-light fw-bold">PŘIHLÁSIT SE</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
